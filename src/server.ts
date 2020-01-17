564685b30dbb3f5cfb85cf4fda82dd188d1113e8
arnold-graf <arnold.graf@gmail.com>
2020-01-17 17:13:19 +0100

    remove that express stuff.

--- src/server.ts
+++ src/server.ts
@@ -1,40 +1,25 @@
-import * as express from 'express'
 import * as socketIo from 'socket.io'
-import * as http from 'http'
 import * as util from 'util'
-import * as cors from 'cors'
 
-const app = express()
-
-app.set('port', 3000)
-
-const corsOptions: cors.CorsOptions = {
-  origin: [
+const websocket = socketIo({
+  origins: [
     'http://localhost:8080',
     'https://transcribe.dioe.at'
   ],
-  credentials: true
-}
-
-app.use(cors(corsOptions))
-
-const server = new http.Server(app)
-const websocket = socketIo(http, { origins: '*:*'})
-
-app.get('/health-check', (req, res) => {
-  res.send('OK!')
+  path: '/updates'
 })
 
 websocket.on('connect', (sock) => {
   console.log('a user connected')
-  console.log(sock.handshake.headers)
+  console.log(sock.handshake.headers.cookie)
   sock.send('hello!')
+  websocket.emit('USER_CONNECTED', {
+    name: 'arni',
+    id: 1
+  })
+  sock.on('disconnect', () => {
+    console.log('disconnected!!')
+  })
 })
 
-websocket.listen(server)
-
-const s = server.listen(3000, function() {
-  console.log('listening on *:3000')
-})
-
-websocket.listen(s)
+websocket.listen(3000)
